c09a7a230df8cbb5e4eba7dcb5131350a0d39d9e
Jindong Yang <yangjindong@gmail.com>
2024-11-02 20:52:29 +0800

    turbo_frame_tag and turbo_stream

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+const colors = require('tailwindcss/colors')
+
+module.exports = {
+  content: [
+    './app/views/**/*.html.erb',
+    './app/helpers/**/*.rb',
+    './app/assets/stylesheets/**/*.css',
+    './app/javascript/**/*.js'
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: colors.rose,
+      }
+    }
+  },
+}
